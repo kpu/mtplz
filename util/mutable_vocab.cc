cab323f7fa307d293518d2e5d880e69524f80298
Kenneth Heafield <github@kheafield.com>
2014-03-27 08:06:43 -0700

    Remove debugging

--- util/mutable_vocab.cc
+++ util/mutable_vocab.cc
@@ -1,7 +1,6 @@
 #include "util/mutable_vocab.hh"
 
 #include "util/murmur_hash.hh"
-#include <iostream>
 
 namespace util {
 
@@ -21,20 +20,14 @@ MutableVocab::ID MutableVocab::Find(const StringPiece &str) const {
 uint32_t MutableVocab::FindOrInsert(const StringPiece &str) {
   MutableVocabInternal entry;
   entry.key = util::MurmurHashNative(str.data(), str.size());
-	//std::cout << "foi1: " << str << std::endl;
   Map::MutableIterator it;
-	//std::cout << "foi1.1: " << str << std::endl;
   if (map_.FindOrInsert(entry, it)) {
-		//std::cout << "foi1.2: " << str << std::endl;
     return it->id;
   }
-	//std::cout << "foi2: " << str << std::endl;
   it->id = strings_.size();
   
-	//std::cout << "foi3: " << str << std::endl;
   char *copied = static_cast<char*>(piece_backing_.Allocate(str.size()));
   memcpy(copied, str.data(), str.size());
-	//std::cout << "foi4: " << str << std::endl;
   strings_.push_back(StringPiece(copied, str.size()));
   return it->id;
 }
