e7eef9784cd63b83b73c7c1828572d48b4e0a809
LarsHuluk <LarsHuluk@gmail.com>
2016-06-14 14:29:41 +0100

    Add lexicalized reordering

--- decode/lexro.cc
+++ decode/lexro.cc
@@ -4,24 +4,44 @@
 namespace decode {
 
 void LexicalizedReordering::Init(FeatureInit &feature_init) {
-  UTIL_THROW_IF(feature_init.PhraseAccess().lexical_reordering, util::Exception,
+  UTIL_THROW_IF(!feature_init.PhraseAccess().lexical_reordering, util::Exception,
       "requested lexicalized reordering but feature values are missing in phrase access");
+  phrase_start_ = util::PODField<std::size_t>(feature_init.HypothesisLayout());
   phrase_access_ = &feature_init.PhraseAccess();
 }
 
 void LexicalizedReordering::ScoreHypothesisWithSourcePhrase(
     const Hypothesis &hypothesis, const SourcePhrase source_phrase, ScoreCollector &collector) const {
-  // TODO calculate
+  phrase_start_(collector.NewHypothesis()) = source_phrase.Span().first;
 }
 
 void LexicalizedReordering::ScoreHypothesisWithPhrasePair(
     const Hypothesis &hypothesis, PhrasePair phrase_pair, ScoreCollector &collector) const {
-  // TODO store for next
+  std::size_t index = FORWARD + PhraseRelation(hypothesis, phrase_pair.source_phrase.Span());
+  float score = phrase_access_->lexical_reordering(&phrase_pair.target_phrase)[index];
+  collector.AddDense(0, score);
 }
 
 void LexicalizedReordering::RescoreHypothesis(
     const Hypothesis &hypothesis, ScoreCollector &collector) const {
-  // TODO execute backward model
+  float score = 0;
+  while (const Hypothesis *prev_hypo = hypothesis.Previous()) {
+    SourceSpan phrase_span = SourceSpan(phrase_start_(&hypothesis), hypothesis.SourceEndIndex());
+    std::size_t index = BACKWARD + PhraseRelation(*prev_hypo, phrase_span);
+    score += phrase_access_->lexical_reordering(hypothesis.Target())[index];
+  }
+  collector.AddDense(1, score);
+}
+
+LexicalizedReordering::Relation LexicalizedReordering::PhraseRelation(
+    const Hypothesis &hypothesis, SourceSpan span) const {
+  if (hypothesis.SourceEndIndex() == span.first) {
+    return MONOTONE;
+  } else if (phrase_start_(&hypothesis) == span.second) {
+    return SWAP;
+  } else {
+    return DISCONTINUOUS;
+  }
 }
 
 std::string LexicalizedReordering::FeatureDescription(std::size_t index) const {
--- decode/lexro.hh
+++ decode/lexro.hh
@@ -6,6 +6,13 @@ namespace decode {
   
 class LexicalizedReordering : public Feature {
   public:
+    typedef std::size_t Relation;
+    constexpr static const std::size_t FORWARD = 0;
+    constexpr static const std::size_t BACKWARD = 3;
+    constexpr static const Relation MONOTONE = 0;
+    constexpr static const Relation SWAP = 1;
+    constexpr static const Relation DISCONTINUOUS = 2;
+
     LexicalizedReordering() : Feature("lexro") {}
 
     void Init(FeatureInit &feature_init) override;
@@ -27,6 +34,9 @@ class LexicalizedReordering : public Feature {
 
     std::string FeatureDescription(std::size_t index) const override;
   private:
+    Relation PhraseRelation(const Hypothesis &hypothesis, SourceSpan span) const;
+
+    util::PODField<std::size_t> phrase_start_;
     const pt::Access *phrase_access_;
 };
 
