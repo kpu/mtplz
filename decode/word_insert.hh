b0bb646c6f3fa224ed9e49df7280284917246761
LarsHuluk <LarsHuluk@gmail.com>
2016-07-21 17:02:30 +0100

    Add word insertion penalty feature

---
+++ decode/word_insert.hh
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "decode/feature.hh"
+
+namespace util { class MutableVocab; }
+
+namespace decode {
+
+class WordInsertion : public Feature {
+  public:
+    WordInsertion() : Feature("WordInsertion") {}
+
+    void Init(FeatureInit &feature_init) override {
+      UTIL_THROW_IF(!feature_init.phrase_access.target, util::Exception,
+          "requested word insertion penalty but target words missing in phrase access");
+      phrase_access_ = feature_init.phrase_access;
+      pt_row_field_ = feature_init.pt_row_field;
+    }
+
+    static const StringPiece Name();
+
+    void NewWord(const StringPiece string_rep, VocabWord *word) const override {}
+
+    void ScorePhrase(PhrasePair phrase_pair, ScoreCollector &collector) const override {
+      collector.AddDense(0, phrase_access_.target(pt_row_field_(phrase_pair.target_phrase)).size());
+    }
+
+    void ScoreHypothesisWithSourcePhrase(
+        const Hypothesis &hypothesis, const SourcePhrase source_phrase, ScoreCollector &collector) const override {}
+
+    void ScoreHypothesisWithPhrasePair(
+        const Hypothesis &hypothesis, PhrasePair phrase_pair, ScoreCollector &collector) const override;
+
+    void ScoreFinalHypothesis(
+        const Hypothesis &hypothesis, ScoreCollector &collector) const override;
+
+    std::size_t DenseFeatureCount() const override { return 1; }
+
+    std::string FeatureDescription(std::size_t index) const override {
+      assert(index == 0);
+      return "word insertion penalty";
+    }
+
+  private:
+    util::PODField<const pt::Row*> pt_row_field_;
+};
+
+} // namespace decode
