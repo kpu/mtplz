10b1bea0da1c824e0954435848b1cbe517ac0061
LarsHuluk <LarsHuluk@gmail.com>
2016-07-22 19:12:27 +0100

    Fix loop conditions for output.cc

--- decode/output.cc
+++ decode/output.cc
@@ -8,12 +8,6 @@
 
 namespace decode {
 
-bool Valid(const TargetPhrase *row) {
-  // TODO replace with proper validity check, if there is such a thing for row
-  // previous: row.Valid()
-  return true;
-}
-
 void PrintOptionalInfo(ScoreHistoryMap &map, float score_delta, util::FileStream &out) {
   map["_total"].scores.push_back(score_delta);
   map["_total"].total += score_delta;
@@ -32,29 +26,26 @@ void PrintOptionalInfo(ScoreHistoryMap &map, float score_delta, util::FileStream
 
 // verbose print
 void Output(const Hypothesis &hypo, const util::MutableVocab &vocab,
-    ScoreHistoryMap &map, util::FileStream &to, const FeatureInit &feature_init,
+    ScoreHistoryMap &map, util::FileStream &out, const FeatureInit &feature_init,
     bool verbose) {
-  // TODO more efficient algorithm?  Also, I wish rope was part of the standard.
   std::vector<const Hypothesis*> hypos;
   for (const Hypothesis *h = &hypo; h; h = h->Previous()) {
-    if (!Valid(h->Target())) continue;
+    if (h->Target() == nullptr) continue;
     hypos.push_back(h);
   }
-  to << hypo.GetScore();
+  out << hypo.GetScore();
   float previous_score = 0.0;
-  assert(feature_init.phrase_access.target); // check that there is a target
-  for (std::vector<const Hypothesis*>::const_reverse_iterator i = hypos.rbegin(); i != hypos.rend() - 1 /* skip EOS */; ++i) {
-    if ((*i)->Target() != nullptr) {
-      auto ids = feature_init.phrase_access.target(feature_init.pt_row_field((*i)->Target()));
-      for (const ID id : ids) {
-        to << ' ' << vocab.String(id);
-      }
+  assert(feature_init.phrase_access.target);
+  for (std::vector<const Hypothesis*>::const_reverse_iterator i = hypos.rbegin(); i != hypos.rend(); ++i) {
+    auto ids = feature_init.phrase_access.target(feature_init.pt_row_field((*i)->Target()));
+    for (const ID id : ids) {
+      out << ' ' << vocab.String(id);
     }
     if (verbose) {
       float this_score = hypo.GetScore();
       float score_delta = this_score - previous_score;
       previous_score = this_score;
-      PrintOptionalInfo(map, score_delta, to);
+      PrintOptionalInfo(map, score_delta, out);
     }
   }
 }
