023f4ac2a8ffe837a3b62c1c5d89eac02963f48e
LarsHuluk <LarsHuluk@gmail.com>
2016-08-03 18:02:42 +0100

    Add phrase count feature

---
+++ decode/phrase_count_feature.hh
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "decode/feature.hh"
+
+namespace decode {
+
+class PhraseCountFeature : public Feature {
+  public:
+    PhraseCountFeature() : Feature("phrase_insertion") {}
+
+    void Init(FeatureInit &feature_init) override {}
+
+    static const StringPiece Name();
+
+    void NewWord(const StringPiece string_rep, VocabWord *word) const override {}
+
+    void InitPassthroughPhrase(pt::Row *passthrough) const override {}
+
+    void ScorePhrase(PhrasePair phrase_pair, ScoreCollector &collector) const override {
+      collector.AddDense(0, 1);
+    }
+
+    void ScoreHypothesisWithSourcePhrase(
+        const Hypothesis &hypothesis, const SourcePhrase source_phrase, ScoreCollector &collector) const override {}
+
+    void ScoreHypothesisWithPhrasePair(
+        const Hypothesis &hypothesis, PhrasePair phrase_pair, ScoreCollector &collector) const override {}
+
+    void ScoreFinalHypothesis(
+        const Hypothesis &hypothesis, ScoreCollector &collector) const override {}
+
+    std::size_t DenseFeatureCount() const override {
+      return 1;
+    }
+
+    std::string FeatureDescription(std::size_t index) const override {
+      assert(index == 0);
+      return "phrase count feature";
+    }
+};
+
+} // namespace decode
